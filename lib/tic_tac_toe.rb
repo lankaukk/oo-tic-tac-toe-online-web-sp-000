faf8cfaf997397f415747d331060880a51a97b23
McKayla Lankau <lankaukk@yahoo.com>
2020-09-19 22:02:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class TicTacToe 
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
-    @WIN_COMIBINATIONS = [
+    @WIN_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
